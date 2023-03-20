214a0ef873c49b850040b15b8d57cff3c8a81c91
bapt2 <5711@holbertonstudents.com>
2023-03-20 19:13:33 +0100

    fixing some issue

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,5 +2,5 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO hbnb_dev;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT on performance_schema.* to 'hbnb_dev'@'localhost';
