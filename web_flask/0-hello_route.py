b37aa425604de83c15e0f906d50b91fb8f6eb9f0
bapt2 <5711@holbertonstudents.com>
2023-04-03 12:04:36 +0200

    adding some documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+""" script that starts a Flask web application"""
 
 from flask import Flask
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+
     return "Hello HBNB!"
 
 if __name__ == '__main__':
